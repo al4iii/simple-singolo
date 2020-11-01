dafcf9a7bf5f3bcec9d1a1ecd48776a850621aee
Aleksandr Danilin <al4icrux@gmail.com>
2020-11-01 11:11:33 +0300

    init: start task

---
+++ script.js
@@ -0,0 +1,26 @@
+// function burgerMenu(selector){
+//   let menu = $(selector);
+//   let buttom = menu.find('.burger-menu__button');
+//   let link = menu.find('.burger-menu__link');
+//   let overlay = menu.find('.burger-menu__overlay')
+
+// button.on('click' (e) => {
+//   e.preventDafault();
+//   toggleMenu();
+// });
+
+// likns.on('click', () => toggleMenu());
+// overlay.on('click', () => toggleMenu());
+
+// function toggleMenu() {
+//   menu.toggeClass('burger-menu_acrive');
+
+//  if (menu.hasClass ('burger-menu_active')){
+//  $('body').css('overflow', 'hidden')
+//  }else {
+//    $ ('body').css('overflow', 'visible');
+//  }
+// }
+// }
+
+// burgerMenu('.burgrer-menu');
