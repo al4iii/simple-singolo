a56aac0508c64036d108e2965cc8162a03a54898
Aliaksandr Danilin <al4icrux@gmail.com>
2020-11-02 17:09:43 +0300

    feat: add burger-menu

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-// function burgerMenu(selector){
-//   let menu = $(selector);
-//   let buttom = menu.find('.burger-menu__button');
-//   let link = menu.find('.burger-menu__link');
-//   let overlay = menu.find('.burger-menu__overlay')
+const all = document.querySelector('.button__all');
+const web = document.querySelector('.button__web-design');
+const graphic = document.querySelector('.button__graphic-gesign');
+const artwork = document.querySelector('.button__artwork');
+const all_img = document.querySelector('.layout-4-column');
+const web_img = all_img.querySelector('.portfolio_web');
+const graphic_img = all_img.querySelector('.portfolio_gesign');
+const artwork_img = all_img.querySelector('.portfolio_artwork');
+const right_chev = document.querySelector('.slider__right-chev');
+const left_chev = document.querySelector('.slider__left-chev');
 
-// button.on('click' (e) => {
-//   e.preventDafault();
-//   toggleMenu();
-// });
 
-// likns.on('click', () => toggleMenu());
-// overlay.on('click', () => toggleMenu());
-
-// function toggleMenu() {
-//   menu.toggeClass('burger-menu_acrive');
-
-//  if (menu.hasClass ('burger-menu_active')){
-//  $('body').css('overflow', 'hidden')
-//  }else {
-//    $ ('body').css('overflow', 'visible');
-//  }
-// }
-// }
-
-// burgerMenu('.burgrer-menu');
+all.addEventListener('click', () => {
+  console.log("привет")
+  web_img.classList.remove('image-hide');
+  graphic_img.classList.remove('image-hide');
+  artwork_img.classList.remove('image-hide');
+});
+web.addEventListener('click', () => {
+  console.log("привет")  
+  web_img.classList.toggle('image-hide');
+});
+graphic.addEventListener('click', () => {
+  console.log("привет")
+  graphic_img.classList.toggle('image-hide');
+});
+artwork.addEventListener('click', () => {
+  console.log("привет")
+  artwork_img.classList.toggle('image-hide');
+});
+right_chev.addEventListener('click', () => {
+  console.log("привет")    
+});
+left_chev.addEventListener('click', () => {
+  console.log("привет")    
+});
