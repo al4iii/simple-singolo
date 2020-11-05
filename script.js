371475d76b7886968b0fd76b7c72a6240b42ca8e
Aliaksandr Danilin <al4icrux@gmail.com>
2020-11-05 16:34:02 +0300

    review-of-fixes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const graphicImg = allImg.querySelector('.portfolio_gesign');
 const artworkImg = allImg.querySelector('.portfolio_artwork');
 const rightChev = document.querySelector('.slider__right-chev');
 const leftChev = document.querySelector('.slider__left-chev');
-
+console.log(webImg)
 
 all.addEventListener('click', () => {   
   webImg.classList.remove('image-hide');
@@ -28,3 +28,39 @@ rightChev.addEventListener('click', () => {
 });
 leftChev.addEventListener('click', () => {     
 });
+
+
+/* Mixed portfolio images */
+
+// const imagesPortfolio = document.getElementById('img_portfolio');
+// const filterList = document.getElementById('tab');
+// let arr1 = imagesPortfolio.querySelectorAll('img');
+// let a = 0;
+// let arr2 = [];
+// for (var i = arr1.length - 1; i >= 0; i--) {
+//   arr2[i] = a;
+// }
+// filterList.querySelectorAll('li').forEach((el) => {
+//   el.addEventListener('click', (event) => {
+//     filterList
+//       .querySelectorAll('li')
+//       .forEach((el) => el.classList.remove('active'));
+//     event.target.classList.add('active');
+//     imagesPortfolio.querySelectorAll('img').forEach((el) => el.remove());
+//     for (var i = arr1.length - 1; i > 0; i--) {
+//       var j = Math.floor(Math.random() * (i + 1));
+//       var temp = arr1[i];
+//       arr1[i] = arr1[j];
+//       arr1[j] = temp;
+//     }
+//     while (i < arr1.length) {
+//       var j = Math.floor(Math.random() * arr1.length);
+//       if (arr2[j] == a) {
+//         arr2[j] = a + 1;
+//         imagesPortfolio.prepend(arr1[j]);
+//         ++i;
+//       }
+//     }
+//     ++a;
+//   });
+// });
